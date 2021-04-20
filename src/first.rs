c28cfa516cfd208f09348ca686bde247ed97629b
Justis <jgipson@codelicious.com>
2021-04-20 09:38:51 -0400

    lists... first lesson done, working on second

--- src/first.rs
+++ src/first.rs
@@ -1,9 +1,85 @@
-struct Node {
-  elem: i32,
-  next: List,
+use std::mem;
+
+pub struct List {
+  head: Link,
 }
 
-pub enum List {
+enum Link {
   Empty,
   More(Box<Node>),
+}
+
+struct Node {
+  elem: i32,
+  next: Link,
+}
+
+impl List {
+  // NOTE: self is kinda an alias for "that type I wrote at the top next to impl... great for keeping it DRY"
+  pub fn new() -> Self {
+    List { head: Link::Empty }
+  }
+
+  pub fn push(&mut self, elem: i32) {
+    let new_node = Box::new(Node {
+        elem: elem,
+        next: mem::replace(&mut self.head, Link::Empty),
+    });
+
+    self.head = Link::More(new_node);
+}
+
+  pub fn pop(&mut self) -> Option<i32> {
+      match mem::replace(&mut self.head, Link::Empty) {
+          Link::Empty => None,
+          Link::More(node) => {
+              self.head = node.next;
+              Some(node.elem)
+          }
+      }
+  }
+}
+
+impl Drop for List {
+  fn drop(&mut self) {
+    let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+
+    while let Link::More(mut boxed_node) = cur_link {
+      cur_link = mem::replace(&mut boxed_node.next, Link::Empty)
+    }
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use super::List;
+
+  #[test]
+  fn basics() {
+    let mut list = List::new();
+
+    // check empty list behaves
+    assert_eq!(list.pop(), None);
+
+    // populate list
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    // check normal removal
+    assert_eq!(list.pop(), Some(3));
+    assert_eq!(list.pop(), Some(2));
+
+    // push more, just to be sure
+    list.push(4);
+    list.push(5);
+
+    // check normal removal
+    assert_eq!(list.pop(), Some(5));
+    assert_eq!(list.pop(), Some(4));
+
+    // check exhaustion
+    assert_eq!(list.pop(), Some(1));
+    assert_eq!(list.pop(), None);
+  }
 }
