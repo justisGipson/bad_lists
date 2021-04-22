83c52b6cbcec8612e9ae876f625dcd6360addc12
Justis <jgipson@codelicious.com>
2021-04-22 10:29:15 -0400

    Iter done + iter tests

--- src/second.rs
+++ src/second.rs
@@ -69,6 +69,33 @@ impl<T> Iterator for IntoIter<T> {
   }
 }
 
+// Iter is generic over *some* lifetime, it doesn't care
+pub struct Iter<'a, T> {
+  next: Option<&'a Node<T>>,
+}
+// No lifetime here, List doesn't have any associated lifetimes
+impl<T> List<T> {
+  // we declare a fresh lifetime here for the *exact* borrow that
+  // creates the Iter. Now &self needs to be valid as long as the
+  // Iter is around
+  pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+      Iter { next: self.head.as_deref() }
+  }
+}
+// We *do* have a lifetime here, because Iter has one that we need to define
+impl<'a, T> Iterator for Iter<'a, T> {
+  // need it here too, this is a type declaration
+  type Item = &'a T;
+  // None of this needs to change, handled by the above.
+  // Self continues to be incredibly hype and amazing
+  fn next(&mut self) -> Option<Self::Item> {
+      self.next.map(|node| {
+          self.next = node.next.as_deref();
+          &node.elem
+      })
+  }
+}
+
 #[cfg(test)]
 mod test {
   use super::List;
@@ -133,5 +160,18 @@ mod test {
     assert_eq!(iter.next(), Some(2));
     assert_eq!(iter.next(), Some(1));
     assert_eq!(iter.next(), None);
+  }
+
+  #[test]
+  fn iter() {
+    let mut list = List::new();
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    let mut iter = list.iter();
+    assert_eq!(iter.next(), Some(&3));
+    assert_eq!(iter.next(), Some(&2));
+    assert_eq!(iter.next(), Some(&1));
   }
 }
