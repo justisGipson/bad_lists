092b47b773955c24af8308383de020ecec1d32a0
Justis <jgipson@codelicious.com>
2021-04-23 16:22:04 -0400

    refactor second.rs

--- src/second.rs
+++ src/second.rs
@@ -24,116 +24,81 @@ impl<T> List<T> {
   }
 
   pub fn pop(&mut self) -> Option<T> {
-     self.head.take().map(|node| {
-       self.head = node.next;
-       node.elem
-     })
+      self.head.take().map(|node| {
+          self.head = node.next;
+          node.elem
+      })
   }
 
   pub fn peek(&self) -> Option<&T> {
-    self.head.as_ref().map(|node| {
-      &node.elem
-    })
+      self.head.as_ref().map(|node| {
+          &node.elem
+      })
   }
 
   pub fn peek_mut(&mut self) -> Option<&mut T> {
-    self.head.as_mut().map(|node| {
-      &mut node.elem
-    })
+      self.head.as_mut().map(|node| {
+          &mut node.elem
+      })
+  }
+
+  pub fn into_iter(self) -> IntoIter<T> {
+      IntoIter(self)
+  }
+
+  pub fn iter(&self) -> Iter<'_, T> {
+      Iter { next: self.head.as_deref() }
+  }
+
+  pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+      IterMut { next: self.head.as_deref_mut() }
   }
 }
 
 impl<T> Drop for List<T> {
   fn drop(&mut self) {
-    let mut cur_link = self.head.take();
-
-    while let Some(mut boxed_node) = cur_link {
-      cur_link = boxed_node.next.take();
-    }
+      let mut cur_link = self.head.take();
+      while let Some(mut boxed_node) = cur_link {
+          cur_link = boxed_node.next.take();
+      }
   }
 }
 
 pub struct IntoIter<T>(List<T>);
 
-impl<T> List<T> {
-  pub fn into_iter(self) -> IntoIter<T> {
-    IntoIter(self)
-  }
-}
-
 impl<T> Iterator for IntoIter<T> {
   type Item = T;
   fn next(&mut self) -> Option<Self::Item> {
-    // access fields of tuple struct numerically
-    self.0.pop()
+      // access fields of a tuple struct numerically
+      self.0.pop()
   }
 }
 
-// Iter is generic over *some* lifetime, it doesn't care
 pub struct Iter<'a, T> {
   next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+  type Item = &'a T;
+  fn next(&mut self) -> Option<Self::Item> {
+      self.next.map(|node| {
+          self.next = node.next.as_deref();
+          &node.elem
+      })
+  }
 }
 
 pub struct IterMut<'a, T> {
   next: Option<&'a mut Node<T>>,
 }
 
-// No lifetime here, List doesn't have any associated lifetimes
-impl<T> List<T> {
-  // we declare a fresh lifetime here for the *exact* borrow that
-  // creates the Iter. Now &self needs to be valid as long as the
-  // Iter is around
-  pub fn iter_mut(&mut self) -> IterMut<'_, T> {
-      IterMut { next: self.head.as_deref_mut() }
-  }
-}
-
-impl<T> List<T> {
-  // we declare a fresh lifetime here for the *exact* borrow that
-  // creates the Iter. Now &self needs to be valid as long as the
-  // Iter is around
-  pub fn iter(&mut self) -> Iter<'_, T> {
-      Iter { next: self.head.as_deref() }
-  }
-}
-
 impl<'a, T> Iterator for IterMut<'a, T> {
   type Item = &'a mut T;
 
   fn next(&mut self) -> Option<Self::Item> {
-    // We take the Option<&mut> so we have exclusive access to the mutable reference. No need to worry about someone looking at it again.
-    self.next.take().map(|node| {
-      self.next = node.next.as_deref_mut();
-      &mut node.elem
-    })
-  }
-}
-
-// Same as:
-// impl<T> List<T> {
-//   pub fn iter<'a>(&'a self) -> Iter<'a, T> {
-//     Iter { next: self.head.as_deref() }
-// }
-// }
-
-// or if not comfortable hiding that a struct contains a lifetime, you can use the
-// "explicitly elided lifetime syntax `_`"
-// impl<T> List<T> {
-//   pub fn iter(&self) -> Iter<'_, T> {
-//       Iter { next: self.head.as_deref() }
-//   }
-// }
-
-// We *do* have a lifetime here, because Iter has one that we need to define
-impl<'a, T> Iterator for Iter<'a, T> {
-  // need it here too, this is a type declaration
-  type Item = &'a T;
-  // None of this needs to change, handled by the above.
-  // Self continues to be incredibly hype and amazing
-  fn next(&mut self) -> Option<Self::Item> {
-      self.next.map(|node| {
-          self.next = node.next.as_deref();
-          &node.elem
+      self.next.take().map(|node| {
+          self.next = node.next.as_deref_mut();
+          &mut node.elem
       })
   }
 }
@@ -144,89 +109,82 @@ mod test {
 
   #[test]
   fn basics() {
-    let mut list = List::new();
+      let mut list = List::new();
 
-    // check empty list behaves
-    assert_eq!(list.pop(), None);
+      // Check empty list behaves right
+      assert_eq!(list.pop(), None);
 
-    // populate list
-    list.push(1);
-    list.push(2);
-    list.push(3);
+      // Populate list
+      list.push(1);
+      list.push(2);
+      list.push(3);
 
-    // check normal removal
-    assert_eq!(list.pop(), Some(3));
-    assert_eq!(list.pop(), Some(2));
+      // Check normal removal
+      assert_eq!(list.pop(), Some(3));
+      assert_eq!(list.pop(), Some(2));
 
-    // push more, just to be sure
-    list.push(4);
-    list.push(5);
+      // Push some more just to make sure nothing's corrupted
+      list.push(4);
+      list.push(5);
 
-    // check normal removal
-    assert_eq!(list.pop(), Some(5));
-    assert_eq!(list.pop(), Some(4));
+      // Check normal removal
+      assert_eq!(list.pop(), Some(5));
+      assert_eq!(list.pop(), Some(4));
 
-    // check exhaustion
-    assert_eq!(list.pop(), Some(1));
-    assert_eq!(list.pop(), None);
+      // Check exhaustion
+      assert_eq!(list.pop(), Some(1));
+      assert_eq!(list.pop(), None);
   }
+
   #[test]
   fn peek() {
-    let mut list = List::new();
-    assert_eq!(list.peek(), None);
-    assert_eq!(list.peek_mut(), None);
-    list.push(1);
-    list.push(2);
-    list.push(3);
+      let mut list = List::new();
+      assert_eq!(list.peek(), None);
+      assert_eq!(list.peek_mut(), None);
+      list.push(1); list.push(2); list.push(3);
 
-    assert_eq!(list.peek(), Some(&3));
-    assert_eq!(list.peek_mut(), Some(&mut 3));
+      assert_eq!(list.peek(), Some(&3));
+      assert_eq!(list.peek_mut(), Some(&mut 3));
 
-    list.peek_mut().map(| value| {
-      *value = 42
-    });
+      list.peek_mut().map(|value| {
+          *value = 42
+      });
 
-    assert_eq!(list.peek(), Some(&42));
-    assert_eq!(list.pop(), Some(42));
+      assert_eq!(list.peek(), Some(&42));
+      assert_eq!(list.pop(), Some(42));
   }
 
   #[test]
   fn into_iter() {
-    let mut list = List::new();
-    list.push(1);
-    list.push(2);
-    list.push(3);
+      let mut list = List::new();
+      list.push(1); list.push(2); list.push(3);
 
-    let mut iter = list.into_iter();
-    assert_eq!(iter.next(), Some(3));
-    assert_eq!(iter.next(), Some(2));
-    assert_eq!(iter.next(), Some(1));
-    assert_eq!(iter.next(), None);
+      let mut iter = list.into_iter();
+      assert_eq!(iter.next(), Some(3));
+      assert_eq!(iter.next(), Some(2));
+      assert_eq!(iter.next(), Some(1));
+      assert_eq!(iter.next(), None);
   }
 
   #[test]
   fn iter() {
-    let mut list = List::new();
-    list.push(1);
-    list.push(2);
-    list.push(3);
+      let mut list = List::new();
+      list.push(1); list.push(2); list.push(3);
 
-    let mut iter = list.iter();
-    assert_eq!(iter.next(), Some(&3));
-    assert_eq!(iter.next(), Some(&2));
-    assert_eq!(iter.next(), Some(&1));
+      let mut iter = list.iter();
+      assert_eq!(iter.next(), Some(&3));
+      assert_eq!(iter.next(), Some(&2));
+      assert_eq!(iter.next(), Some(&1));
   }
 
   #[test]
   fn iter_mut() {
-    let mut list = List::new();
-    list.push(1);
-    list.push(2);
-    list.push(3);
+      let mut list = List::new();
+      list.push(1); list.push(2); list.push(3);
 
-    let mut iter = list.iter_mut();
-    assert_eq!(iter.next(), Some(&mut 3));
-    assert_eq!(iter.next(), Some(&mut 2));
-    assert_eq!(iter.next(), Some(&mut 1));
+      let mut iter = list.iter_mut();
+      assert_eq!(iter.next(), Some(&mut 3));
+      assert_eq!(iter.next(), Some(&mut 2));
+      assert_eq!(iter.next(), Some(&mut 1));
   }
 }
