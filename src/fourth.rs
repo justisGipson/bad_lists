166ca719a0d1c03dd92bbd5890cca4181d0cbaff
Justis <justis710@gmail.com>
2021-05-07 12:32:30 -0400

    a bad safe deque - peek done

--- src/fourth.rs
+++ src/fourth.rs
@@ -15,7 +15,7 @@
 // Alright, we want to be doubly-linked. This means each node has a pointer to the previous and next node. Also, the list itself has a pointer to the first and last node. This gives us fast insertion and removal on both ends of the list.
 
 use std::rc::Rc;
-use std::cell::RefCell;
+use std::cell::{Ref, RefCell};
 
 pub struct List<T> {
   head: Link<T>,
@@ -75,6 +75,12 @@ impl<T> List<T> {
       Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
     })
   }
+
+  pub fn peek_front(&self) -> Option<Ref<T>>{
+    self.head.as_ref().map(|node| {
+      Ref::map(node.borrow(), |node| &node.elem)
+    })
+  }
 }
 
 impl<T> Drop for List<T>{
@@ -108,5 +114,16 @@ mod test {
     // check exhaustion
     assert_eq!(list.pop_front(), Some(1));
     assert_eq!(list.pop_front(), None);
+  }
+
+  #[test]
+  fn peek() {
+    let mut list = List::new();
+    assert!(list.peek_front().is_none());
+    list.push_front(1);
+    list.push_front(2);
+    list.push_front(3);
+
+    assert_eq!(&*list.peek_front().unwrap(), &3);
   }
 }
